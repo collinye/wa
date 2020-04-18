2f20829e0aea02f9ab3e8404a4b280026ce601b6
wadahana <wadahana@gmail.com>
2020-04-18 14:42:17 +0800

    fix:  rename liftClick() to leftClick()

--- mouse.go
+++ mouse.go
@@ -26,12 +26,12 @@ func MouseClickPos(x, y int32) {
 func MouseClick(args ...string) {
 	switch len(args) {
 	case 0:
-		liftClick()
+		leftClick()
 		return
 	case 1:
 		switch args[0] {
 		case "left":
-			liftClick()
+			leftClick()
 			return
 		case "right":
 			rightClick()
@@ -39,15 +39,36 @@ func MouseClick(args ...string) {
 		}
 	case 2:
 		if args[0] == "left" && args[1] == "double" {
-			liftClick()
-			liftClick()
+			leftClick()
+			leftClick()
 			return
 		}
 		return
 	}
 }
+/* button :
+	0 Left;  1 Middle; 2 Right
+*/
+func SetMouseKey(button int, press bool) {
+	flags := [6]uint32{mouseEventLeftUp, mouseEventLeftDown, 
+					   mouseEventMiddleUp, mouseEventMiddleDown,
+				       mouseEventRightUp, mouseEventRightDown};
 
-func liftClick() {
+	if button < 0 || button > 2 {
+		return
+	}
+	var input mouseInput
+	index := 0;
+	if press {
+		index = 1
+	}
+	index = button * 2 + index;
+	input.Mi.DwFlags = flags[index]
+	log.Debugf("SetMouseKey: button(%d), dwFlags (%08x)", index, flags[index]);
+	sendInputa(1, unsafe.Pointer(&input), int32(unsafe.Sizeof(input)))
+}
+
+func leftClick() {
 	var input, input2 mouseInput
 	input.Mi.DwFlags = mouseEventLeftDown
 	input2.Mi.DwFlags = mouseEventLeftUp
